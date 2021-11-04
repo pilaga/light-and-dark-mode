3c3bc417ed826a86277a1dba1a3382e7d7eee891
Pierre <pierre.lagadec@gmail.com>
2021-11-04 12:38:06 +1100

    toggle theme with javascscript

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-/*
-dark
-https://colorhunt.co/palette/1f1d363f3351864879e9a6a6
-#1F1D36;
-#3F3351;
-#864879;
-#E9A6A6;
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
 
-light
---maximum-blue-green: #17bebbff;
---raisin-black: #2e282aff;
---pastel-pink: #e9a6a6ff;
---straw: #dbd56eff;
---light-goldenrod-yellow: #f2f6d0ff;
-https://coolors.co/17bebb-2e282a-e9a6a6-dbd56e-f2f6d0
-*/
+//event listener
+toggleSwitch.addEventListener('change', switchTheme);
+
+//swich theme dynamically
+function switchTheme(event) {
+    if(event.target.checked) { //switch to dark monde
+        document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }
+}
